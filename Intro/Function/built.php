8a492852c4b31253ebf8cc8528f108f583ada8c0
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-06-23 11:44:02 +0600

    new

--- Intro/Function/built.php
+++ Intro/Function/built.php
@@ -8,10 +8,11 @@
 <body>
     <h1>Built in Function</h1>
     <?php 
-        print strlen("Country");
+        
+        print strlen("Country"); // string length
         echo "<br>";
         function name(){
-            return substr("Bangladesh",0, 10); 
+            return substr("Bangladesh",0, 10); // (sring,start, length)
         }
         print name();
         print "<br>";
@@ -21,8 +22,26 @@
         # int $start, int $length = null): string
         print "<br>";
         $text = "Hello, I am learning php to increase my knowledge."; 
-        print str_word_count($text);
+        print str_word_count($text); //string word count
+        echo "<br>";
+        echo strrev($text); // string reverse
+        echo "<br>";
+        echo strtolower($text);
+        echo "<br>";
+        echo strtoupper($text);
+        echo "<br>";
+        function name1(){
+        $st = "This is php module and is add another module mysql.". "". "<br>";
+            return str_repeat($st ,3); // string repeat 
+            };
+           echo name1();
+        function name2(){
+            $sst = "this is php module and is add another module mysql.";
+            return str_replace("php","javascript", $sst); // str replace 
+        };
+        echo name2();
+
+       
     ?>
-    
 </body>
 </html>
